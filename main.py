2d740458e729069a5fe5579f0790a493335c2ff6
dandytron <104713695+dandytron@users.noreply.github.com>
2024-03-06 12:08:48 +0800

    Switched over to a hash table, made functions simpler

--- main.py
+++ main.py
@@ -2,44 +2,45 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    letter_count = count_letters(text)
-    letter_count.sort(reverse=True, key=sort_on)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = dict_to_sorted_list(chars_dict)
+
     print(f"--- Begin report of {book_path} ---")
     print(f"{word_count} words found in this document\n\n")
-    for letter in letter_count:
-        print(f"The '{letter["letter"]}' character was found {letter["num"]} times")
 
+    for char in chars_sorted_list:
+        if not char["char"].isalpha():
+            continue
+        print(f"The '{char["char"]}' character was found {char["num"]} times")
+
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def dict_to_sorted_list(dict):
+    sorted_list = []
+    for char in dict:
+        sorted_list.append({"char": char, "num": dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def count_words(text):
-    words = text.split()
-    word_counter = 0
-    for word in words:
-        word_counter += 1
-    return word_counter
-
-def count_letters(text):
-    lowered_letters = text.lower()
-    letter_count_list = []
-    for char in lowered_letters:
-        if char.isalpha():
-            is_in_list = False
-            for dictionary in letter_count_list:
-                if dictionary["letter"] == char:
-                    dictionary["num"] += 1
-                    is_in_list = True
-            if is_in_list == False:
-                new_char = {
-                    "letter": char,
-                    "num": 1
-                }
-                letter_count_list.append(new_char)
-    return letter_count_list
-
-def sort_on(dict):
-    return dict["num"]
 
 main()
